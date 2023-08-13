7d84457cef4c5da8800e3e7aa820a098f23e3dbf
Dalveer Singh <57098009+dalveer16@users.noreply.github.com>
2023-08-13 08:24:34 +0530

    error handling in loader

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,22 +1,30 @@
 import { useLoaderData } from "react-router-dom";
-
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  if (data.isError) {
+    return <p>{data.msg}</p>;
+  }
+  const events = data.events;
 
   return <EventsList events={events} />;
 }
 
 export default EventsPage;
 
+//this loader is only running on browser
+//and we need to write the funtionallity
+//available in browser
+//eg : local storage , cookies
+//but not useState as it a react hook
 export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // ...
+    return { isError: true, msg: "Something went wrong" };
   } else {
     const resData = await response.json();
-    return resData.events;
+    return resData;
   }
 }
